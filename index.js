c62dd1cd7f6d1506a53d900c92070e6e0e26e28b
mdmoreau <mdmoreau@gmail.com>
2016-06-02 16:48:52 -0400

    add youtube and vimeo support

--- index.js
+++ index.js
@@ -32,12 +32,30 @@ function minimodal(target) {
     _.closeButton.addEventListener('click', _.close);
   };
 
-  _.url = function() {
-    return _.current.getAttribute('href');
+  _.youtube = function() {
+    var id = _.url.split('v=')[1];
+    return '<div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen>';
+  };
+
+  _.vimeo = function() {
+    var id = _.url.split('vimeo.com/')[1];
+    return '<div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://player.vimeo.com/video/' + id + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>';
+  };
+
+  _.image = function() {
+    return '<img class="minimodal__element" src="' + _.url + '">';
   };
 
   _.load = function() {
-    var element = '<img class="minimodal__element" src="' + _.url() + '">';
+    var element;
+    _.url = _.current.getAttribute('href');
+    if (_.url.indexOf('youtube.com') > -1) {
+      element = _.youtube();
+    } else if (_.url.indexOf('vimeo.com') > -1) {
+      element = _.vimeo();
+    } else {
+      element = _.image();
+    }
     _.item = _.node('<div class="minimodal__item"><div class="minimodal__content">' + element);
     _.viewport.appendChild(_.item);
   };
