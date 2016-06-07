e4026bade2e41cbe7f97baf1f3f7201226185944
mdmoreau <mdmoreau@gmail.com>
2016-06-07 19:11:05 -0400

    add umd support

--- index.js
+++ index.js
@@ -1,227 +1,241 @@
-function minimodal(target, options) {
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.minimodal = factory();
+  }
+}(this, function() {
 
-  options = typeof options !== 'undefined' ? options : {};
+  var minimodal = function(target, options) {
 
-  var _ = {};
+    options = typeof options !== 'undefined' ? options : {};
 
-  _.current = target;
+    var _ = {};
 
-  _.options = {
-    closeTimeout: typeof options.closeTimeout !== 'undefined' ? options.closeTimeout : 0,
-    statusTimeout: typeof options.statusTimeout !== 'undefined' ? options.statusTimeout : 0,
-    removeTimeout: typeof options.removeTimeout !== 'undefined' ? options.removeTimeout : 0
-  };
+    _.current = target;
 
-  _.node = function(html) {
-    var div = document.createElement('div');
-    div.innerHTML = html;
-    return div.firstChild;
-  };
+    _.options = {
+      closeTimeout: typeof options.closeTimeout !== 'undefined' ? options.closeTimeout : 0,
+      statusTimeout: typeof options.statusTimeout !== 'undefined' ? options.statusTimeout : 0,
+      removeTimeout: typeof options.removeTimeout !== 'undefined' ? options.removeTimeout : 0
+    };
 
-  _.setup = function() {
-    _.minimodal = _.node('<div class="minimodal" tabindex="0">');
-    _.overlay = _.node('<div class="minimodal__overlay">');
-    _.viewport = _.node('<div class="minimodal__viewport">');
-    _.closeButton = _.node('<button class="minimodal__close">Close</button>');
-  };
+    _.node = function(html) {
+      var div = document.createElement('div');
+      div.innerHTML = html;
+      return div.firstChild;
+    };
 
-  _.build = function() {
-    _.minimodal.appendChild(_.overlay);
-    _.minimodal.appendChild(_.viewport);
-    _.minimodal.appendChild(_.closeButton);
-    document.body.appendChild(_.minimodal);
-    _.minimodal.focus();
-    _.minimodal.classList.add('minimodal--active');
-  };
+    _.setup = function() {
+      _.minimodal = _.node('<div class="minimodal" tabindex="0">');
+      _.overlay = _.node('<div class="minimodal__overlay">');
+      _.viewport = _.node('<div class="minimodal__viewport">');
+      _.closeButton = _.node('<button class="minimodal__close">Close</button>');
+    };
 
-  _.close = function() {
-    var minimodal = _.minimodal;
-    minimodal.classList.remove('minimodal--active');
-    setTimeout(function() {
-      if (minimodal.parentNode) {
-        minimodal.parentNode.removeChild(minimodal);
-      }
-    }, _.options.closeTimeout);
-    document.removeEventListener('keydown', _.keydown);
-    target.focus();
-  };
+    _.build = function() {
+      _.minimodal.appendChild(_.overlay);
+      _.minimodal.appendChild(_.viewport);
+      _.minimodal.appendChild(_.closeButton);
+      document.body.appendChild(_.minimodal);
+      _.minimodal.focus();
+      _.minimodal.classList.add('minimodal--active');
+    };
 
-  _.focusTrap = function(e) {
-    if (e.shiftKey) {
-      if (_.minimodal === document.activeElement) {
-        e.preventDefault();
-        _.closeButton.focus();
-      }
-    } else {
-      if (_.closeButton === document.activeElement) {
-        e.preventDefault();
-        _.minimodal.focus();
-      }
-    }
-  };
+    _.close = function() {
+      var minimodal = _.minimodal;
+      minimodal.classList.remove('minimodal--active');
+      setTimeout(function() {
+        if (minimodal.parentNode) {
+          minimodal.parentNode.removeChild(minimodal);
+        }
+      }, _.options.closeTimeout);
+      document.removeEventListener('keydown', _.keydown);
+      target.focus();
+    };
 
-  _.keydown = function(e) {
-    if (e.keyCode === 9) {
-      _.focusTrap(e);
-    } else if (e.keyCode === 27) {
-      _.close();
-    } else if (e.keyCode === 37) {
-      if (_.index > -1) {
-        _.previous();
-      }
-    } else if (e.keyCode === 39) {
-      if (_.index > -1) {
-        _.next();
-      }
-    }
-  };
-
-  _.listen = function() {
-    _.closeButton.addEventListener('click', _.close);
-    document.addEventListener('keydown', _.keydown);
-  };
-
-  _.reflow = function() {
-    var x = _.minimodal.clientWidth;
-  };
-
-  _.loading = function() {
-    _.status = _.node('<div class="minimodal__status">Loading</div>');
-    _.item.appendChild(_.status);
-    _.reflow();
-    _.item.classList.add('minimodal__item--loading');
-  };
-
-  _.loaded = function() {
-    var status = _.status;
-    setTimeout(function() {
-      if (status.parentNode) {
-        status.parentNode.removeChild(status);
-      }
-    }, _.options.statusTimeout);
-    _.item.appendChild(_.content);
-    _.item.classList.remove('minimodal__item--loading');
-    _.reflow();
-    _.item.classList.add('minimodal__item--loaded');
-  };
-
-  _.error = function() {
-    _.status.innerHTML = 'Error loading resource';
-  };
-
-  _.youtube = function() {
-    var id = _.url.split('v=')[1];
-    _.content = _.node('<div class="minimodal__content"><div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen>');
-    _.loaded();
-  };
-
-  _.vimeo = function() {
-    var id = _.url.split('vimeo.com/')[1];
-    _.content = _.node('<div class="minimodal__content"><div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://player.vimeo.com/video/' + id + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>');
-    _.loaded();
-  };
-
-  _.image = function() {
-    var img = document.createElement('img');
-    img.onload = function() {
-      if (img.src.indexOf(_.url) > -1) {
-        _.content = _.node('<div class="minimodal__content"><img class="minimodal__element" src="' + _.url + '">');
-        _.loaded();
+    _.focusTrap = function(e) {
+      if (e.shiftKey) {
+        if (_.minimodal === document.activeElement) {
+          e.preventDefault();
+          _.closeButton.focus();
+        }
+      } else {
+        if (_.closeButton === document.activeElement) {
+          e.preventDefault();
+          _.minimodal.focus();
+        }
       }
     };
-    img.onerror = function() {
-      if (img.src.indexOf(_.url) > -1) {
-        _.error();
+
+    _.keydown = function(e) {
+      if (e.keyCode === 9) {
+        _.focusTrap(e);
+      } else if (e.keyCode === 27) {
+        _.close();
+      } else if (e.keyCode === 37) {
+        if (_.index > -1) {
+          _.previous();
+        }
+      } else if (e.keyCode === 39) {
+        if (_.index > -1) {
+          _.next();
+        }
       }
     };
-    img.src = _.url;
-  };
 
-  _.load = function(type) {
-    _.url = _.current.getAttribute('href');
-    _.item = _.node('<div class="minimodal__item">');
-    _.viewport.appendChild(_.item);
-    if (type) {
-      _.item.classList.add('minimodal__item--added');
-      _.item.classList.add('minimodal__item--added--' + type);
+    _.listen = function() {
+      _.closeButton.addEventListener('click', _.close);
+      document.addEventListener('keydown', _.keydown);
+    };
+
+    _.reflow = function() {
+      var x = _.minimodal.clientWidth;
+    };
+
+    _.loading = function() {
+      _.status = _.node('<div class="minimodal__status">Loading</div>');
+      _.item.appendChild(_.status);
       _.reflow();
-      _.item.classList.remove('minimodal__item--added');
-      _.item.classList.remove('minimodal__item--added--' + type);
-    }
-    _.loading();
-    if (_.url.indexOf('youtube.com') > -1) {
-      _.youtube();
-    } else if (_.url.indexOf('vimeo.com') > -1) {
-      _.vimeo();
-    } else {
-      _.image();
-    }
-  };
+      _.item.classList.add('minimodal__item--loading');
+    };
 
-  _.remove = function(type) {
-    var item = _.item;
-    item.classList.add('minimodal__item--removed');
-    item.classList.add('minimodal__item--removed--' + type);
-    setTimeout(function() {
-      if (item.parentNode) {
-        item.parentNode.removeChild(item);
+    _.loaded = function() {
+      var status = _.status;
+      setTimeout(function() {
+        if (status.parentNode) {
+          status.parentNode.removeChild(status);
+        }
+      }, _.options.statusTimeout);
+      _.item.appendChild(_.content);
+      _.item.classList.remove('minimodal__item--loading');
+      _.reflow();
+      _.item.classList.add('minimodal__item--loaded');
+    };
+
+    _.error = function() {
+      _.status.innerHTML = 'Error loading resource';
+    };
+
+    _.youtube = function() {
+      var id = _.url.split('v=')[1];
+      _.content = _.node('<div class="minimodal__content"><div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen>');
+      _.loaded();
+    };
+
+    _.vimeo = function() {
+      var id = _.url.split('vimeo.com/')[1];
+      _.content = _.node('<div class="minimodal__content"><div class="minimodal__element minimodal__element--video"><iframe class="minimodal__video" src="https://player.vimeo.com/video/' + id + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>');
+      _.loaded();
+    };
+
+    _.image = function() {
+      var img = document.createElement('img');
+      img.onload = function() {
+        if (img.src.indexOf(_.url) > -1) {
+          _.content = _.node('<div class="minimodal__content"><img class="minimodal__element" src="' + _.url + '">');
+          _.loaded();
+        }
+      };
+      img.onerror = function() {
+        if (img.src.indexOf(_.url) > -1) {
+          _.error();
+        }
+      };
+      img.src = _.url;
+    };
+
+    _.load = function(type) {
+      _.url = _.current.getAttribute('href');
+      _.item = _.node('<div class="minimodal__item">');
+      _.viewport.appendChild(_.item);
+      if (type) {
+        _.item.classList.add('minimodal__item--added');
+        _.item.classList.add('minimodal__item--added--' + type);
+        _.reflow();
+        _.item.classList.remove('minimodal__item--added');
+        _.item.classList.remove('minimodal__item--added--' + type);
       }
-    }, _.options.removeTimeout);
-  };
-
-  _.update = function(type) {
-    _.remove(type);
-    _.current = _.groupItems[_.index];
-    _.load(type);
-  };
-
-  _.previous = function() {
-    if (_.index - 1 < 0) {
-      _.index = _.indexMax;
-    } else {
-      _.index -= 1;
-    }
-    _.update('previous');
-  };
-
-  _.next = function() {
-    if (_.index + 1 > _.indexMax) {
-      _.index = 0;
-    } else {
-      _.index += 1;
-    }
-    _.update('next');
-  };
-
-  _.nav = function() {
-    _.previousButton = _.node('<button class="minimodal__nav minimodal__nav--previous">Previous</button>');
-    _.nextButton = _.node('<button class="minimodal__nav minimodal__nav--next">Next</button>');
-    _.minimodal.insertBefore(_.previousButton, _.closeButton);
-    _.minimodal.insertBefore(_.nextButton, _.closeButton);
-    _.previousButton.addEventListener('click', _.previous);
-    _.nextButton.addEventListener('click', _.next);
-  };
-
-  _.group = function() {
-    _.groupID = _.current.getAttribute('data-minimodal');
-    if (_.groupID) {
-      _.groupItems = document.querySelectorAll('[data-minimodal="' + _.groupID + '"]');
-      if (_.groupItems.length > 1) {
-        _.index = Array.prototype.indexOf.call(_.groupItems, _.current);
-        _.indexMax = _.groupItems.length - 1;
-        _.nav();
+      _.loading();
+      if (_.url.indexOf('youtube.com') > -1) {
+        _.youtube();
+      } else if (_.url.indexOf('vimeo.com') > -1) {
+        _.vimeo();
+      } else {
+        _.image();
       }
-    }
+    };
+
+    _.remove = function(type) {
+      var item = _.item;
+      item.classList.add('minimodal__item--removed');
+      item.classList.add('minimodal__item--removed--' + type);
+      setTimeout(function() {
+        if (item.parentNode) {
+          item.parentNode.removeChild(item);
+        }
+      }, _.options.removeTimeout);
+    };
+
+    _.update = function(type) {
+      _.remove(type);
+      _.current = _.groupItems[_.index];
+      _.load(type);
+    };
+
+    _.previous = function() {
+      if (_.index - 1 < 0) {
+        _.index = _.indexMax;
+      } else {
+        _.index -= 1;
+      }
+      _.update('previous');
+    };
+
+    _.next = function() {
+      if (_.index + 1 > _.indexMax) {
+        _.index = 0;
+      } else {
+        _.index += 1;
+      }
+      _.update('next');
+    };
+
+    _.nav = function() {
+      _.previousButton = _.node('<button class="minimodal__nav minimodal__nav--previous">Previous</button>');
+      _.nextButton = _.node('<button class="minimodal__nav minimodal__nav--next">Next</button>');
+      _.minimodal.insertBefore(_.previousButton, _.closeButton);
+      _.minimodal.insertBefore(_.nextButton, _.closeButton);
+      _.previousButton.addEventListener('click', _.previous);
+      _.nextButton.addEventListener('click', _.next);
+    };
+
+    _.group = function() {
+      _.groupID = _.current.getAttribute('data-minimodal');
+      if (_.groupID) {
+        _.groupItems = document.querySelectorAll('[data-minimodal="' + _.groupID + '"]');
+        if (_.groupItems.length > 1) {
+          _.index = Array.prototype.indexOf.call(_.groupItems, _.current);
+          _.indexMax = _.groupItems.length - 1;
+          _.nav();
+        }
+      }
+    };
+
+    _.open = function() {
+      _.setup();
+      _.build();
+      _.listen();
+      _.load();
+      _.group();
+    };
+
+    return _;
+
   };
 
-  _.open = function() {
-    _.setup();
-    _.build();
-    _.listen();
-    _.load();
-    _.group();
-  };
+  return minimodal;
 
-  return _;
-
-}
+}));
